31878f5bfd8f01016e34bbe49f60b8b2fee7b88b
stepantishhen <stepantishhen@mail.ru>
2023-07-03 15:47:42 +0300

    students_assignments page

--- lms/urls.py
+++ lms/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('all_courses/', all_courses), # ../lms/all_couses/
     path('course/<int:course_id>', course_single),
     path('my_courses/', my_courses),
-    path('course/<int:course_id>/module/<int:module_id>',  module_single),
+    path('course/<int:course_id>/module/<int:module_id>',  module_single, name='module_single'),
     path('assignments/',  assignments),
 ]
