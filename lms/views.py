587f377cf8d9a695aa3bde7216bcae8fd0495a5d
elenagernichenko <elenagernichenko@yandex.ru>
2023-07-03 16:23:16 +0300

    added my_courses page functional

--- lms/views.py
+++ lms/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import HomeworkAnswer, Course
+
 from django.views.generic import ListView
 
 from lms.models import Module
@@ -12,9 +15,33 @@ def all_courses(request):
 def course_single(request, course_id):
     return render(request, 'lms/course_page_notenroll.html')
 
-
+@login_required
 def my_courses(request):
-    return HttpResponse('trash Список моих курсов')
+    user = request.user
+    enrolled_courses = user.enrolled_courses.all()
+    homeworks_per_course = {}
+
+    for course in enrolled_courses:
+        homework_answers = HomeworkAnswer.objects.filter(user=user, homework__course=course, status='rat')
+        print(homework_answers)
+        count = homework_answers.count()
+
+        modules_count = Module.objects.filter(course=course).count()
+        print(modules_count)
+
+        if modules_count > 0:
+            percent = (count / modules_count) * 100
+        else:
+            percent = 0
+        homeworks_per_course[course] = percent
+    print(homeworks_per_course)
+    context = {
+        'homeworks_per_course': homeworks_per_course
+    }
+
+    return render(request,
+                  'lms/my_courses.html',
+                  context=context)
 
 
 def module_single(request, course_id, module_id):
