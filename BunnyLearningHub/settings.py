eb25ec62c4e930d438725cc61838c424ff4160cc
elenagernichenko <elenagernichenko@yandex.ru>
2023-07-04 13:23:56 +0300

    edited progress bar, added login_url in settings.py

--- BunnyLearningHub/settings.py
+++ BunnyLearningHub/settings.py
@@ -134,3 +134,5 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'admin:login'
--- lms/views.py
+++ lms/views.py
@@ -7,14 +7,14 @@ from django.views.generic import ListView
 
 from lms.models import *
 
-
+@login_required
 def all_courses(request):
     context = {
         'courses': Course.objects.all()
     }
     return render(request, 'lms/all_courses.html', context=context)
 
-
+@login_required
 def course_single(request, course_id):
     course = Course.objects.get(id=course_id)
     context = {
@@ -32,18 +32,16 @@ def my_courses(request):
 
     for course in enrolled_courses:
         homework_answers = HomeworkAnswer.objects.filter(user=user, homework__course=course, status='rat')
-        print(homework_answers)
         count = homework_answers.count()
 
         modules_count = Module.objects.filter(course=course).count()
-        print(modules_count)
 
         if modules_count > 0:
             percent = (count / modules_count) * 100
         else:
             percent = 0
         homeworks_per_course[course] = percent
-    print(homeworks_per_course)
+
     context = {
         'homeworks_per_course': homeworks_per_course
     }
@@ -52,11 +50,11 @@ def my_courses(request):
                   'lms/my_courses.html',
                   context=context)
 
-
+@login_required
 def module_single(request, course_id, module_id):
     return HttpResponse('Курс номер: ' + str(course_id) + " модуль: " + str(module_id))
 
-
+@login_required
 def assignments(request):
     user = request.user
     assignments = HomeworkAnswer.objects.filter(user=user)
