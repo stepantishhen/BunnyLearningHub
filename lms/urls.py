f444400a4d8a691920d0b5b8bc5229e272208a8e
elenagernichenko <elenagernichenko@yandex.ru>
2023-07-04 10:41:08 +0300

    added my_courses link to navbar; edited button label in my_courses page; added links to course single pages in my_courses page

--- lms/urls.py
+++ lms/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from lms.views import *
 
 urlpatterns = [
-    path('all_courses/', all_courses), # ../lms/all_couses/
+    path('all_courses/', all_courses, name='all_courses'), # ../lms/all_couses/
     path('course/<int:course_id>', course_single, name='course_single'),
     path('my_courses/', my_courses, name='my_courses'),
     path('course/<int:course_id>/module/<int:module_id>',  module_single, name='module_single'),
