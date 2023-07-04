0710e73ab17e74dabd4cf1d217381346af40a95d
elenagernichenko <elenagernichenko@yandex.ru>
2023-07-04 21:48:46 +0300

    исправила проблему с неотображением курсов на my_courses, если у модулей отсутсвовал homework

--- lms/views.py
+++ lms/views.py
@@ -34,33 +34,22 @@ def my_courses(request):
     enrolled_courses = user.enrolled_courses.all()
     homeworks_per_course = {}
 
-    course_data = []
     for course in enrolled_courses:
-        homework_count = HomeworkAnswer.objects.filter(user=user, homework__course=course, status='rat').count()
-        module_count = Module.objects.filter(course=course).count()
+        modules = Module.objects.filter(course=course)
+        homework_count = 0
+        module_count = modules.count()
+
+        for module in modules:
+            module_homework_count = HomeworkAnswer.objects.filter(user=user, homework__module=module,
+                                                                  status='rat').count()
+            if module_homework_count > 0:
+                homework_count += module_homework_count
 
         if module_count != 0:
             percent = (homework_count / module_count) * 100
         else:
             percent = 0
         homeworks_per_course[course] = int(percent)
-
-        course_data.append({
-            'course': course,
-            'homework_count': homework_count,
-            'module_count': module_count,
-            'percent': percent
-        })
-
-    for course_data in course_data:
-        course = course_data['course']
-        homework_count = course_data['homework_count']
-        module_count = course_data['module_count']
-
-        print(f"Курс: {course.name}")
-        print(f"Количество модулей: {module_count}")
-        print(f"Количество оцененных домашних заданий: {homework_count}")
-        print()
 
     context = {
         'homeworks_per_course': homeworks_per_course
